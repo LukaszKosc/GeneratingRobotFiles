3d7eaf27d25c27b4c0834b8af76b5ae7d5032eb2
Lukasz Koscielny <mail@domena.com>
2017-03-28 18:00:39 +0200

    Added JSON_manipulation tool - do not loose it!

---
+++ JSON_manipulation.py
@@ -0,0 +1,375 @@
+
+json = {
+    "app":{
+        "childrenArray" : [
+            {
+                "_d": {
+                    "Flowers0": {
+                        "Red flower": "Rose",
+                        "White Flower": "Jasmine0",
+                        "Yellow Flower": "Marigold"
+                    }
+                },
+                "_ds": {
+                    "Yellow fruit": "Mango",
+                    "Green fruit": "Guava",
+                    "White Flower": "groovy0"
+                },
+                "Trees0": {
+                    "label": {
+                        "Yellow fruit": "Pumpkin",
+                        "White Flower": "Bogan0"
+                    }
+                },
+                'element_key': {"childrenArray": [
+                    {
+                        "_d": {
+                            "Flowers01": {
+                                "Red flower": "Rose",
+                                "White Flower": "Jasmine01",
+                                "Yellow Flower": "Marigold"
+                            }
+                        },
+                        "_ds": {
+                            "Yellow fruit": "Mango",
+                            "Green fruit": "Guava",
+                            "White Flower": "groovy01"
+                        },
+                        "Trees0": {
+                            "label": {
+                                "Yellow fruit": "Pumpkin",
+                                "White Flower": "Bogan01"
+                            }
+                        },
+                        'reall_deep_shit':{"childrenArray" : [
+            {
+                "_d": {
+                    "Flowers0": {
+                        "Red flower": "Rose",
+                        "White Flower": "Jasmine0",
+                        "Yellow Flower": "Marigold"
+                    }
+                },
+                "_ds": {
+                    "Yellow fruit": "Mango",
+                    "Green fruit": "Guava",
+                    "White Flower": "groovy0"
+                },
+                "Trees0": {
+                    "label": {
+                        "Yellow fruit": "Pumpkin",
+                        "White Flower": "Bogan0"
+                    }
+                },
+                'element_key': {"childrenArray": [
+                    {
+                        "_d": {
+                            "Flowers01": {
+                                "Red flower": "Rose",
+                                "White Flower": "Jasmine01",
+                                "Yellow Flower": "Marigold"
+                            }
+                        },
+                        "_ds": {
+                            "Yellow fruit": "Mango",
+                            "Green fruit": "Guava",
+                            "White Flower": "groovy01"
+                        },
+                        "Trees0": {
+                            "label": {
+                                "Yellow fruit": "Pumpkin",
+                                "White Flower": "Bogan01"
+                            }
+                        }
+                    },
+                    {
+                        "_d": {
+                            "Flowers1": {
+                                "textValue": "text1",
+                                "Red flower": "Rose",
+                                "White Flower": "Jasmine11",
+                                "Yellow Flower": "Marigold"
+                            }
+                        },
+                        "_ds": {
+                            "textValue": "text2",
+                            "Yellow fruit": "Mango",
+                            "Green fruit": "Guava",
+                            "White Flower": "groovy11"
+                        },
+                        "Trees1": {
+                            "label": {
+                                "textValue": "text3",
+                                "Yellow fruit": "Pumpkin",
+                                "White Flower": "Bogan11"
+                            }
+                        }
+                    }
+                ]},'another_deep':{
+                "appesika": {
+                    "childrenArray": [
+                        {
+                            "_d": {
+                                "Flowers0": {
+                                    "Red flower": "Rose",
+                                    "White Flower": "Jasmine0",
+                                    "Yellow Flower": "Marigold"
+                                }
+                            },
+                            "_ds": {
+                                "Yellow fruit": "Mango",
+                                "Green fruit": "Guava",
+                                "White Flower": "groovy0"
+                            },
+                            "Trees0": {
+                                "label": {
+                                    "Yellow fruit": "Pumpkin",
+                                    "White Flower": "Bogan0"
+                                }
+                            },
+                            'element_key': {"childrenArray": [
+                                {
+                                    "_d": {
+                                        "Flowers01": {
+                                            "Red flower": "Rose",
+                                            "White Flower": "Jasmine01",
+                                            "Yellow Flower": "Marigold"
+                                        }
+                                    },
+                                    "_ds": {
+                                        "Yellow fruit": "Mango",
+                                        "Green fruit": "Guava",
+                                        "White Flower": "groovy01"
+                                    },
+                                    "Trees0": {
+                                        "label": {
+                                            "Yellow fruit": "Pumpkin",
+                                            "White Flower": "Bogan01"
+                                        }
+                                    },
+                                    'reall_deep_shit': {"childrenArray": [
+                                        {
+                                            "_d": {
+                                                "Flowers0": {
+                                                    "Red flower": "Rose",
+                                                    "White Flower": "Jasmine0",
+                                                    "Yellow Flower": "Marigold"
+                                                }
+                                            },
+                                            "_ds": {
+                                                "Yellow fruit": "Mango",
+                                                "Green fruit": "Guava",
+                                                "White Flower": "groovy0"
+                                            },
+                                            "Trees0": {
+                                                "label": {
+                                                    "Yellow fruit": "Pumpkin",
+                                                    "White Flower": "Bogan0"
+                                                }
+                                            },
+                                            'element_key': {"childrenArray": [
+                                                {
+                                                    "_d": {
+                                                        "Flowers01": {
+                                                            "Red flower": "Rose",
+                                                            "White Flower": "Jasmine01",
+                                                            "Yellow Flower": "Marigold"
+                                                        }
+                                                    },
+                                                    "_ds": {
+                                                        "Yellow fruit": "Mango",
+                                                        "Green fruit": "Guava",
+                                                        "White Flower": "groovy01"
+                                                    },
+                                                    "Trees0": {
+                                                        "label": {
+                                                            "Yellow fruit": "Pumpkin",
+                                                            "White Flower": "Bogan01"
+                                                        }
+                                                    }
+                                                },
+                                                {
+                                                    "_d": {
+                                                        "Flowers1": {
+                                                            "textValue": "text1",
+                                                            "Red flower": "Rose",
+                                                            "White Flower": "Jasmine11",
+                                                            "Yellow Flower": "Marigold"
+                                                        }
+                                                    },
+                                                    "_ds": {
+                                                        "textValue": "text2",
+                                                        "Yellow fruit": "Mango",
+                                                        "Green fruit": "Guava",
+                                                        "White Flower": "groovy11"
+                                                    },
+                                                    "Trees1": {
+                                                        "label": {
+                                                            "textValue": "text3",
+                                                            "Yellow fruit": "Pumpkin",
+                                                            "White Flower": "Bogan11"
+                                                        }
+                                                    }
+                                                }
+                                            ]}
+                                        },
+                                        {
+                                            "_d": {
+                                                "Flowers1": {
+                                                    "Red flower": "Rose",
+                                                    "White Flower": "Jasmine1",
+                                                    "Yellow Flower": "Marigold"
+                                                }
+                                            },
+                                            "_ds": {
+                                                "Yellow fruit": "Mango",
+                                                "Green fruit": "Guava",
+                                                "White Flower": "groovy1"
+                                            },
+                                            "Trees1": {
+                                                "label": {
+                                                    "Yellow fruit": "Pumpkin",
+                                                    "White Flower": "Bogan1"
+                                                }
+                                            }
+                                        }
+                                    ]}
+                                },
+                                {
+                                    "_d": {
+                                        "Flowers1": {
+                                            "textValue": "text1",
+                                            "Red flower": "Rose",
+                                            "White Flower": "Jasmine11",
+                                            "Yellow Flower": "Marigold"
+                                        }
+                                    },
+                                    "_ds": {
+                                        "textValue": "text2",
+                                        "Yellow fruit": "Mango",
+                                        "Green fruit": "Guava",
+                                        "White Flower": "groovy11"
+                                    },
+                                    "Trees1": {
+                                        "label": {
+                                            "textValue": "text3",
+                                            "Yellow fruit": "Pumpkin",
+                                            "White Flower": "Bogan11"
+                                        }
+                                    }
+                                }
+                            ]}
+                        },
+                        {
+                            "_d": {
+                                "Flowers1": {
+                                    "textValue": "cokolwiek",
+                                    "Red flower": "Rose",
+                                    "White Flower": "Jasmine1",
+                                    "Yellow Flower": "Marigold"
+                                }
+                            },
+                            "_ds": {
+                                "Yellow fruit": "Mango",
+                                "textValue": "text3",
+                                "Green fruit": "Guava",
+                                "White Flower": "groovy1"
+                            },
+                            "Trees1": {
+                                "label": {
+                                    "Yellow fruit": "Pumpkin",
+                                    "White Flower": "Bogan1",
+                                    "textValue": ""
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+            },
+            {
+                "_d": {
+                    "Flowers1": {
+                        "Red flower": "Rose",
+                        "White Flower": "Jasmine1",
+                        "Yellow Flower": "Marigold"
+                    }
+                },
+                "_ds": {
+                    "Yellow fruit": "Mango",
+                    "Green fruit": "Guava",
+                    "White Flower": "groovy1"
+                },
+                "Trees1": {
+                    "label": {
+                        "Yellow fruit": "Pumpkin",
+                        "White Flower": "Bogan1"
+                    }
+                }
+            }
+        ]}
+                    },
+                    {
+                        "_d": {
+                            "Flowers1": {
+                                "textValue": "text1",
+                                "Red flower": "Rose",
+                                "White Flower": "Jasmine11",
+                                "Yellow Flower": "Marigold"
+                            }
+                        },
+                        "_ds": {
+                            "textValue": "text2",
+                            "Yellow fruit": "Mango",
+                            "Green fruit": "Guava",
+                            "White Flower": "groovy11"
+                        },
+                        "Trees1": {
+                            "label": {
+                                "textValue": "text3",
+                                "Yellow fruit": "Pumpkin",
+                                "White Flower": "Bogan11"
+                            }
+                        }
+                    }
+                ]}
+            },
+            {
+                "_d": {
+                    "Flowers1": {
+                        "Red flower": "Rose",
+                        "White Flower": "Jasmine1",
+                        "Yellow Flower": "Marigold"
+                    }
+                },
+                "_ds": {
+                    "Yellow fruit": "Mango",
+                    "Green fruit": "Guava",
+                    "White Flower": "groovy1"
+                },
+                "Trees1": {
+                    "label": {
+                        "Yellow fruit": "Pumpkin",
+                        "White Flower": "Bogan1"
+                    }
+                }
+            }
+        ]
+    }
+}
+
+def find(element_to_find, JSON, path, all_paths):
+    if element_to_find in JSON:
+        path = path + element_to_find + ' = ' + JSON[element_to_find].encode('utf-8')
+        print path
+        all_paths.append(path)
+    for key in JSON:
+        if isinstance(JSON[key], dict):
+            find(element_to_find, JSON[key],path + key + '.',all_paths)
+        if isinstance(JSON[key], list):
+            for element_to_find_to_check in JSON[key]:
+                find(element_to_find, element_to_find_to_check, path + key + '[' + str(JSON[key].index(element_to_find_to_check))+']' + '.', all_paths)
+
+
+all_paths = []
+find('textValue', json,'', all_paths)
