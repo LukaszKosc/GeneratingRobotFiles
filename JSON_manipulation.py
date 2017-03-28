42b6e149036a29539756705690640fb078518e2a
Lukasz Koscielny <mail@domena.com>
2017-03-28 22:06:30 +0200

    Updated code

--- JSON_manipulation.py
+++ JSON_manipulation.py
@@ -373,3 +373,125 @@ def find(element_to_find, JSON, path, all_paths):
 
 all_paths = []
 find('textValue', json,'', all_paths)
+
+
+
+
+# def find(element, JSON, path, all_paths):
+#     if element in JSON:
+#         path = path + element + " = " + JSON[element].encode("utf-8")
+#         print path
+#         all_paths.append(path)
+#     for key in JSON:
+#         if isinstance(JSON[key], dict):
+#             find(element, JSON[key],path + key + ".",all_paths)
+#         if isinstance(JSON[key], list):
+#             for element_to_check in JSON[key]:
+#                 find(element, element_to_check, path + key + "[" + str(JSON[key].index(element_to_check))+"]" + ".", all_paths)
+
+# Uzyj tej - wynik wyglada odrobine lepiej i nie wysypuje sie na slownikach jesli trzeba je wyswietlic
+def find(element, JSON, path, all_paths):
+    if element in JSON:
+        if isinstance(JSON[element], dict):
+            path = path + element + " = " + '{}'.format(JSON[element]) + ';'
+        else:
+            path = path + element + " = " + JSON[element].encode("utf-8") + ';'
+
+        print 'full path: ', path
+        all_paths.append(path)
+    for key in JSON:
+        if isinstance(JSON[key], dict):
+            find(element, JSON[key],path + key + ".",all_paths)
+        if isinstance(JSON[key], list):
+            for element_to_check in JSON[key]:
+                find(element, element_to_check, path + key + "[" + str(JSON[key].index(element_to_check))+"]" + ".", all_paths)
+
+# all_paths = []
+# find("textValue", json,"", all_paths)
+
+
+def show_dict(dictionary, output):
+    for key in dictionary:
+        print '{}: {}'.format(key, dictionary[key])
+        if isinstance(dictionary[key], dict):
+            return output + '\r\nkey: "{}" -> value: "{}"'.format(key, dictionary[key]) + show_dict(dictionary[key], output)
+        elif isinstance(dictionary[key], list):
+            output = output + '\r\nkey: "{}" -> value: "{}"'.format(key, dictionary[key])
+            for element in dictionary[key]:
+                output = output + show_dict(element, output)
+            return output
+        else:
+            return '\r\nkey: "{}" -> value: "{}"'.format(key, dictionary[key])
+
+
+def new_find(element, JSON, path, all_paths):
+    if element in JSON:
+        if isinstance(JSON[element], dict):
+            path = path + element + " = " + '{}'.format(JSON[element])
+            other_path = JSON[element]
+        else:
+            path = path + element + " = " + JSON[element].encode("utf-8")
+            other_path = {}
+
+        print 'new_find: full path: ', path
+        print show_dict(other_path, '').lstrip('\r\n')
+        all_paths.append(path)
+    for key in JSON:
+        if isinstance(JSON[key], dict):
+            new_find(element, JSON[key],path + key + ".",all_paths)
+        if isinstance(JSON[key], list):
+            for element_to_check in JSON[key]:
+                new_find(element, element_to_check, path + key + "[" + str(JSON[key].index(element_to_check))+"]" + ".", all_paths)
+
+
+def get_ancestor_and_all_elements_below(key_to_find, levels_up, JSON):
+    all_paths = []
+    find(key_to_find, JSON, "", all_paths)
+    for path in all_paths:
+
+        if key_to_find in path:
+            number = path.count(key_to_find)
+            if number >= 2:
+                paths = [path for path in path.split(';') if path]
+                print 'paths', paths
+                index = 0
+                for path in paths:
+                    parents_line, children_of_found_element = path.split(" = ")
+                    parents_line = parents_line.split(".")
+                    fiding_index = parents_line.index(key_to_find)
+                    children_of_ancestor = parents_line[fiding_index - levels_up:]
+                    print '-------------------------'
+                    print 'Found element "{}" at index: {}'.format(key_to_find, fiding_index)
+                    if levels_up < fiding_index:
+                        print '{} level{} up: ancestor: {}'.format(levels_up, 's' if levels_up > 2 else '',
+                                                                   '.'.join(parents_line[:fiding_index - levels_up]))
+                    print '\r\nChildren of ancestor at {} levels up: {}'.format(levels_up, '.'.join(
+                        children_of_ancestor) + ' ' + children_of_found_element)
+                    output = ''
+                    print show_dict({parents_line[fiding_index - levels_up]: children_of_found_element}, output)
+                    print '-------------------------'
+                    index += 1
+                index = 0
+            else:
+                print 'SINGLE: path', path
+                print 'key_to_find', key_to_find
+                parents_line, children_of_found_element = path.split(" = ")
+                parents_line = parents_line.split(".")
+                fiding_index = parents_line.index(key_to_find)
+                children_of_ancestor = parents_line[fiding_index-levels_up:]
+                print '-------------------------'
+                print 'element "{}", index: {}'.format(key_to_find, fiding_index)
+                if levels_up < fiding_index:
+                    print '{} level{} up: ancestor: {}'.format(levels_up, 's' if levels_up > 2 else '', '.'.join(parents_line[:fiding_index-levels_up]))
+                print 'children of ancestor at {} levels up: {}'.format(levels_up, '.'.join(children_of_ancestor) + ' '+ children_of_found_element)
+                output = ''
+                print show_dict({parents_line[fiding_index-levels_up]:children_of_found_element}, output)
+                print '-------------------------'
+            # pp = pprint.PrettyPrinter(indent=4)
+            # pp.pprint(children_of_ancestor)
+
+# all_paths = []
+# find("textValue", json,"", all_paths)
+# find("_ds", json,"", all_paths)
+# new_find("_ds", json,"", all_paths)
+# get_ancestor_and_all_elements_below("_d", 1, json)
